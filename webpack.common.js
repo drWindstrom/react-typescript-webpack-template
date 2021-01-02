9718051b42e8c4360fee2a64e9ca90296818c233
fred <jan.winstroth@outlook.com>
2021-01-02 14:20:16 +0100

    Working fine, but without hot reload or react refresh

---
+++ webpack.common.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: "./src/index.tsx",
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      favicon: 'public/favicon.ico',
+      template: 'public/index.html',
+    }),
+  ],
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "build"),
+  },
+  resolve: {
+    // Leave off the extension when importing, order determines resolve order
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.json']
+  },
+  module: {
+    rules: [
+      {
+        // Include ts, tsx, js, and jsx files.
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      // Load images
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        exclude: /node_modules/,
+        type: "asset/resource",
+      },
+      // Load fonts
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        exclude: /node_modules/,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
